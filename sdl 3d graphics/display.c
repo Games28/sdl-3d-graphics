6cd68f9a8cd29105c8a34b6301cda46d3523aa5d
jedi <jake.demko@yahoo.com>
2024-06-06 23:52:21 -0500

    currnent

--- sdl 3d graphics/display.c
+++ sdl 3d graphics/display.c
@@ -20,7 +20,7 @@ bool initialize_window(void) {
     SDL_DisplayMode display_mode;
     SDL_GetCurrentDisplayMode(0, &display_mode);
     //window_width = display_mode.w;
-   // window_height = display_mode.h;
+    //window_height = display_mode.h;
 
     // Create a SDL Window
     window = SDL_CreateWindow(
